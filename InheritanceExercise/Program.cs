f7d98a770330ac89b4376d7da8ca8d2805600267
Owen <owen.bayliss@sky.com>
2022-09-18 10:40:43 +0100

    Add project files.

---
+++ InheritanceExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritanceExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
